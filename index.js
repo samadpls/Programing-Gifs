eb0c4ee87a95448660763b6109776f3998f76e59
Abdul Samad Siddiqui <abdulsamadsid1@gmail.com>
2023-01-29 19:43:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ var http = require("http");
 var fs = require("fs");
 const testFolder = './static/gifs';
 
-const server = http.createServer((request, response) => {
-  if (request.url == "/") {
-    fs.readdir(testFolder, (err, files) => {
-      var fileName = files[Math.floor(Math.random() * files.length)];
-      fs.readFile("./static/gifs/" + fileName, function(err, data) {
-        if (err) {
-          response.writeHead(404);
-          response.end();
-          return;
-        }
-  
+const server=http.createServer((request,response)=>{
+// If the request is for the root url "/", serve the image file
+if (request.url == "/") {
+// Read the image file from the filesystem
+fs.readdir(testFolder,(err,files)=>{
+var fileName=files[Math.floor(Math.random()*files.length)];
+console.log(fileName)
+fs.readFile("./static/gifs/"+fileName, function(err, data) {
+if (err) {
+// If there was an error reading the file, send a 404 Not Found response
+response.writeHead(404);
+response.end();
+return;
+}
         response.writeHead(200, {
           "Content-Type": "image/gif",
           "Cache-Control": "no-cache, no-store, must-revalidate",
